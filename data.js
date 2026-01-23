427b1c11ef484b14cfb3c60583c7bf4c71ee983f
dsssbsmartnotes <nishmit901@gmail.com>
2026-01-23 05:29:43 -0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+// data.js
+const testDatabase = [
+  {
+    "id": "t1",
+    "title": "English PYQ Test – 01",
+    "category": "english",
+    "url": "https://dsssbsmartnotes.github.io/dsssb-mts-cbt/eng01-10-25p12ndsft.html",
+    "badge": "HOT",
+    "attempts": "3240"
+  },
+  {
+    "id": "t2",
+    "title": "GK PYQ Test – 01",
+    "category": "gk",
+    "url": "https://dsssbsmartnotes.github.io/dsssb-mts-cbt/DSSSB%20gk%20pyq.html",
+    "badge": "",
+    "attempts": "1980"
+  },
+  {
+    "id": "t3",
+    "title": "Maths PYQ Test – 01",
+    "category": "maths",
+    "url": "https://dsssbsmartnotes.github.io/dsssb-mts-cbt/Math.html",
+    "badge": "NEW",
+    "attempts": "150"
+  },
+  {
+    "id": "t4",
+    "title": "Reasoning Mock – 01",
+    "category": "reasoning",
+    "url": "mock_reasoning_01.html",
+    "badge": "MOCK",
+    "attempts": "500"
+  }
+];
